fa783e649e75644872951797c1673187f0fe47c4
Ramin Rezaei <ramin12207@gmail.com>
2025-07-10 01:59:53 +0330

    feat: upgrade docusaurus to V 3.8.1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,47 +1,51 @@
-import {themes as prismThemes} from 'prism-react-renderer'
-import type {Config} from '@docusaurus/types'
-import type * as Preset from '@docusaurus/preset-classic'
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'WaterWall',
-  tagline: 'WaterWall tunneling solutions',
-  favicon: 'img/favicon.ico',
+  future: {
+    v4: true,
+    experimental_faster: true,
+  },
+  title: "WaterWall",
+  tagline: "WaterWall tunneling solutions",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://radkesvat.github.io',
+  url: "https://radkesvat.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/WaterWall-Docs',
+  baseUrl: "/WaterWall-Docs",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'radkesvat', // Usually your GitHub org/user name.
-  projectName: 'radkesvat.github.io', // Usually your repo name.
-  deploymentBranch: 'main',
+  organizationName: "radkesvat", // Usually your GitHub org/user name.
+  projectName: "radkesvat.github.io", // Usually your repo name.
+  deploymentBranch: "main",
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   markdown: {
     mermaid: true,
   },
-  themes: ['@docusaurus/theme-mermaid'],
+  themes: ["@docusaurus/theme-mermaid"],
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'fa',
-    locales: ['fa'],
+    defaultLocale: "fa",
+    locales: ["fa"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
 
@@ -56,7 +60,7 @@ const config: Config = {
         //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         // },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -64,37 +68,37 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'WaterWall',
+      title: "WaterWall",
       logo: {
-        alt: 'WaterWall Logo',
-        src: 'img/logo.svg',
+        alt: "WaterWall Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/radkesvat/WaterWall",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: "Tutorial",
+              to: "/docs/intro",
             },
           ],
         },
@@ -116,15 +120,15 @@ const config: Config = {
         //   ],
         // },
         {
-          title: 'More',
+          title: "More",
           items: [
             // {
             //   label: 'Blog',
             //   to: '/blog',
             // },
             {
-              label: 'GitHub',
-              href: 'https://github.com/radkesvat/WaterWall/',
+              label: "GitHub",
+              href: "https://github.com/radkesvat/WaterWall/",
             },
           ],
         },
@@ -136,6 +140,6 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
-}
+};
 
-export default config
+export default config;
